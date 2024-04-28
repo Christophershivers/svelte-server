697525a7c92bc610a29e9eefc16af66a617b568c
Christopher Shivers <chrisshivers55@gmail.com>
2024-04-28 15:20:45 -0500

    removed conflicting routes

--- src/routes/api/user/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler, RequestEvent } from './$types';
-
-export const GET: RequestHandler = async (e: RequestEvent) => {
-    return json(
-        {parameter: "test"}
-    )
-}
