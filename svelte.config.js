ec498238d3a45ec80212e044d4c8c29d3af46b62
Christopher Shivers <chrisshivers55@gmail.com>
2024-04-28 19:45:10 -0500

    removed azure stuff

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,16 +11,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-		pages: 'build',
-		assets: 'build',
-		fallback: undefined,
-		precompress: false,
-		strict: true
-	}),
-	paths: {
-		relative: false
-	}
+    adapter: adapter(),
   }
 };
 
